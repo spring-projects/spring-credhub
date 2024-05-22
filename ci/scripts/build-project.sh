f12292c78be5720eff644150e1800482e128ca53
Gareth Clay <gclay@vmware.com>
2024-05-22 10:24:06 +0100

    Migrate to new image repositories

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -2,15 +2,33 @@
 
 set -euo pipefail
 
+readonly DOCKERHUB_MIRROR_REGISTRY="${DOCKERHUB_MIRROR_REGISTRY:?must be set}"
+readonly DOCKERHUB_MIRROR_REGISTRY_USERNAME="${DOCKERHUB_MIRROR_REGISTRY_USERNAME:?must be set}"
+readonly DOCKERHUB_MIRROR_REGISTRY_PASSWORD="${DOCKERHUB_MIRROR_REGISTRY_PASSWORD:?must be set}"
+
 # shellcheck source=common.sh
 source "$(dirname "$0")/common.sh"
 repository=$(pwd)/distribution-repository
 
 start_docker() {
   pushd credhub-server >/dev/null
-    echo '{"registry-mirrors": ["https://harbor-mirror.spring.vmware.com"]}' > /etc/docker/daemon.json
+    echo "{\"registry-mirrors\": [\"https://$DOCKERHUB_MIRROR_REGISTRY\"]}" > /etc/docker/daemon.json
     service cgroupfs-mount start
     service docker start
+
+    # Work around https://github.com/moby/moby/issues/30880
+    cat >> /etc/hosts << EOF
+127.0.0.1  index.docker.io
+127.0.0.1  registry-1.docker.io
+127.0.0.1  docker.io
+EOF
+    mkdir -p "$HOME/.docker"
+    jq --arg username "$DOCKERHUB_MIRROR_REGISTRY_USERNAME" \
+      --arg password "$DOCKERHUB_MIRROR_REGISTRY_PASSWORD" \
+      'reduce .[] as $registry ({"auths": {}}; .auths += {($registry): {"auth": [$username, $password] | join(":") | @base64}})' \
+      <<< "[\"index.docker.io\", \"$DOCKERHUB_MIRROR_REGISTRY\"]" \
+      > "$HOME/.docker/config.json"
+
     docker-compose up --detach
     trap "stop_docker" EXIT
   popd >/dev/null

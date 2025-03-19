3ee9d601da18a0bbbafe9f3b962ca76e9d69b04e
Roy Clarkson <roy.clarkson@broadcom.com>
2025-03-19 15:29:03 -0400

    Remove out of support versions from CI configuration

--- scripts/set-pipelines.sh
+++ scripts/set-pipelines.sh
@@ -41,11 +41,9 @@ set_pipelines() {
 	fly -t "$FLY_TARGET" sync
   set_pipeline spring-credhub pipeline.yml 3.3.x
   set_pipeline spring-credhub pipeline.yml 3.2.x
-  set_pipeline spring-credhub pipeline.yml 2.3.x
 
   set_pipeline spring-credhub-pr pr-pipeline.yml 3.3.x
   set_pipeline spring-credhub-pr pr-pipeline.yml 3.2.x
-  set_pipeline spring-credhub-pr pr-pipeline.yml 2.3.x
 }
 
 main() {
